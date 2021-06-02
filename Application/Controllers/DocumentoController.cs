efdf3a4460253db93a007e3eec1b0b3b23b2f975
Edson Cuno <edson.cuno@gmx.com>
2021-06-01 21:29:15 -0500

    se terminos los dos casos de uso pero hay unos cambios para la sengunda iteracion

---
+++ Application/Controllers/DocumentoController.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+using Microsoft.EntityFrameworkCore;
+using Application.Models;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace Application.Controllers
+{
+    public class DocumentoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DocumentoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Enviar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Enviar([FromForm] List<IFormFile> archivos, string observaciones, int pedidoId)
+        {
+            List<Documento> documentos = new List<Documento>();
+            string rutaParaDocumento = "C:\\Users\\Edson Cuno\\Downloads\\LOGANTA\\Application\\wwwroot\\documento\\";
+            if (archivos.Count >0)
+            {
+                foreach (IFormFile archivo in archivos) {
+                    string rutaCompleta = Path.Combine(rutaParaDocumento,archivo.FileName);
+                    FileStream fileStream;
+                    using (fileStream = new FileStream(Path.Combine(rutaParaDocumento,Path.GetFileName(archivo.FileName)), FileMode.Create))
+                    {
+                        archivo.CopyTo(fileStream);
+                    }
+                    Documento objDocumento = new Documento();
+                    objDocumento.Observaciones = observaciones;
+                    objDocumento.Fecha = DateTime.Now;
+                    objDocumento.Ruta = rutaCompleta;
+                    objDocumento.PedidoId = pedidoId;
+                    documentos.Add(objDocumento);
+                }
+                foreach (var documento in documentos)
+                {
+                    _context.Add(documento);
+                }
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction(nameof(Index));
+        }
+        // GET: Documento
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Documento.Include(d => d.Pedido);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Documento/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var documento = await _context.Documento
+                .Include(d => d.Pedido)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (documento == null)
+            {
+                return NotFound();
+            }
+
+            return View(documento);
+        }
+
+        // GET: Documento/Create
+        public IActionResult Create()
+        {
+            ViewData["PedidoId"] = new SelectList(_context.Pedido, "Id", "Id");
+            return View();
+        }
+
+        // POST: Documento/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Fecha,Ruta,Observaciones,PedidoId")] Documento documento)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(documento);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PedidoId"] = new SelectList(_context.Pedido, "Id", "Id", documento.PedidoId);
+            return View(documento);
+        }
+
+        // GET: Documento/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var documento = await _context.Documento.FindAsync(id);
+            if (documento == null)
+            {
+                return NotFound();
+            }
+            ViewData["PedidoId"] = new SelectList(_context.Pedido, "Id", "Id", documento.PedidoId);
+            return View(documento);
+        }
+
+        // POST: Documento/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Fecha,Ruta,Observaciones,PedidoId")] Documento documento)
+        {
+            if (id != documento.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(documento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DocumentoExists(documento.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PedidoId"] = new SelectList(_context.Pedido, "Id", "Id", documento.PedidoId);
+            return View(documento);
+        }
+
+        // GET: Documento/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var documento = await _context.Documento
+                .Include(d => d.Pedido)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (documento == null)
+            {
+                return NotFound();
+            }
+
+            return View(documento);
+        }
+
+        // POST: Documento/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var documento = await _context.Documento.FindAsync(id);
+            _context.Documento.Remove(documento);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DocumentoExists(int id)
+        {
+            return _context.Documento.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Application/Controllers/PedidoController.cs
@@ -0,0 +1,344 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Application.Models;
+
+namespace Application.Controllers
+{
+    public class PedidoController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PedidoController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> ListarPendientes()
+        {
+            var todosLosPedidosPendientes = from pedido in _context.Pedido where pedido.PedidoEstadoId == 1 select pedido;
+            return View(await todosLosPedidosPendientes.ToListAsync());
+        }
+        [HttpPost]
+        public async Task<IActionResult> ListarPendientes(string filtro, string valor)
+        {
+            var listaDePedidosPendientes = from pedido in _context.Pedido where pedido.PedidoEstadoId == 1 select pedido;
+            if (filtro == null)
+            {
+                return View(await listaDePedidosPendientes.ToListAsync());
+            }
+            else
+            {
+                if (filtro == "CÓDIGO")
+                {
+                    try
+                    {
+                        int codigo = Convert.ToInt32(valor);
+                        listaDePedidosPendientes = from pedido in _context.Pedido where pedido.PedidoEstadoId == 1 && pedido.Id >= codigo select pedido;
+                        return View(await listaDePedidosPendientes.ToListAsync());
+                    }
+                    catch
+                    {
+                        return View(await listaDePedidosPendientes.ToListAsync());
+                    }
+                }
+                else { 
+                    if(filtro == "ACTIVIDAD OPERATIVA")
+                    {
+                        listaDePedidosPendientes = from pedido in _context.Pedido where pedido.PedidoEstadoId == 1 && pedido.ActividadOperativa == valor select pedido;
+                        return View(await listaDePedidosPendientes.ToListAsync());
+                    }
+                    else
+                    {
+                        if(filtro == "TIPO DE USO")
+                        {
+                            listaDePedidosPendientes = from pedido in _context.Pedido where pedido.PedidoEstadoId == 1 && pedido.TipoDeUso == valor select pedido;
+                            return View(await listaDePedidosPendientes.ToListAsync());
+                        }
+                        else
+                        {
+                            try
+                            {
+                                DateTime fecha = Convert.ToDateTime(valor);
+                                listaDePedidosPendientes = from pedido in _context.Pedido where pedido.PedidoEstadoId == 1 && pedido.Fecha >= fecha select pedido;
+                                return View(await listaDePedidosPendientes.ToListAsync());
+                            }
+                            catch
+                            {
+                                return View(await listaDePedidosPendientes.ToListAsync());
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        public async Task<IActionResult> MostrarPendiente(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedido
+                .Include(p => p.PedidoEstado)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            return View(pedido);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> AprobarPedidoSolicitante(int id)
+        {
+            var pedidoQueQuiero = from pedido in _context.Pedido where pedido.Id == id select pedido;
+            Pedido objPedido = new Pedido();
+            objPedido = pedidoQueQuiero.Single();
+            objPedido.PedidoEstadoId = 2;
+            try
+            {
+                _context.Update(objPedido);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PedidoExists(objPedido.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(ListarPendientes));
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DesaprobarPedidoSolicitante(int id, string observaciones1)
+        {
+            var pedidoQueQuiero = from pedido in _context.Pedido where pedido.Id == id select pedido;
+            Pedido objPedido = new Pedido();
+            objPedido = pedidoQueQuiero.Single();
+            objPedido.PedidoEstadoId = 3;
+            objPedido.Observaciones1 = observaciones1;
+            try
+            {
+                _context.Update(objPedido);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PedidoExists(objPedido.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return RedirectToAction(nameof(ListarPendientes));
+        }
+        public JsonResult ObtenerItems(int id)
+        {
+            var itemsDelPedido = from objItem in _context.Item join objItemXPedido in _context.ItemXPedido on objItem.Id equals objItemXPedido.ItemId join objPedido in _context.Pedido on objItemXPedido.PedidoId equals objPedido.Id where objPedido.Id == id select new { objItem.Id, objItem.Descripcion, objItemXPedido.Cantidad, objItem.UnidadDeMedida };
+            return Json(itemsDelPedido);
+        }
+        public JsonResult ObtenerPedidos(string filtro, string valor)
+        {
+            var listaDePedidos = from pedido in _context.Pedido select pedido;
+            if (filtro == null)
+            {
+                return Json(listaDePedidos);
+            }
+            else
+            {
+                if (filtro == "Código")
+                {
+                    try
+                    {
+                        int codigo = Convert.ToInt32(valor);
+                        listaDePedidos = from pedido in _context.Pedido where pedido.Id >= codigo select pedido;
+                        return Json(listaDePedidos);
+                    }
+                    catch
+                    {
+                        return Json(listaDePedidos);
+                    }
+                }
+                else
+                {
+                    if (filtro == "ActividadOperativa")
+                    {
+                        listaDePedidos = from pedido in _context.Pedido where pedido.ActividadOperativa == valor select pedido;
+                        return Json(listaDePedidos);
+                    }
+                    else
+                    {
+                        if (filtro == "TIPO DE USO")
+                        {
+                            listaDePedidos = from pedido in _context.Pedido where pedido.TipoDeUso == valor select pedido;
+                            return Json(listaDePedidos);
+                        }
+                        else
+                        {
+                            try
+                            {
+                                DateTime fecha = Convert.ToDateTime(valor);
+                                listaDePedidos = from pedido in _context.Pedido where pedido.Fecha >= fecha select pedido;
+                                return Json(listaDePedidos);
+                            }
+                            catch
+                            {
+                                return Json(listaDePedidos);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // GET: Pedido
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Pedido.Include(p => p.PedidoEstado);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Pedido/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedido
+                .Include(p => p.PedidoEstado)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            return View(pedido);
+        }
+
+        // GET: Pedido/Create
+        public IActionResult Create()
+        {
+            ViewData["PedidoEstadoId"] = new SelectList(_context.PedidoEstado, "Id", "Id");
+            return View();
+        }
+
+        // POST: Pedido/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,DireccionDeSolicitante,TipoDeUso,EntregarA,ActividadOperativa,Motivo,Fecha,Observaciones1,Observaciones2,PedidoEstadoId")] Pedido pedido)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(pedido);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PedidoEstadoId"] = new SelectList(_context.PedidoEstado, "Id", "Id", pedido.PedidoEstadoId);
+            return View(pedido);
+        }
+
+        // GET: Pedido/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedido.FindAsync(id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+            ViewData["PedidoEstadoId"] = new SelectList(_context.PedidoEstado, "Id", "Id", pedido.PedidoEstadoId);
+            return View(pedido);
+        }
+
+        // POST: Pedido/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,DireccionDeSolicitante,TipoDeUso,EntregarA,ActividadOperativa,Motivo,Fecha,Observaciones1,Observaciones2,PedidoEstadoId")] Pedido pedido)
+        {
+            if (id != pedido.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(pedido);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!PedidoExists(pedido.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["PedidoEstadoId"] = new SelectList(_context.PedidoEstado, "Id", "Id", pedido.PedidoEstadoId);
+            return View(pedido);
+        }
+
+        // GET: Pedido/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var pedido = await _context.Pedido
+                .Include(p => p.PedidoEstado)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            return View(pedido);
+        }
+
+        // POST: Pedido/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var pedido = await _context.Pedido.FindAsync(id);
+            _context.Pedido.Remove(pedido);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool PedidoExists(int id)
+        {
+            return _context.Pedido.Any(e => e.Id == id);
+        }
+    }
+}
